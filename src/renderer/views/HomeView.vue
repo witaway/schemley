f5a139e15e960584fb1848689eacf121b63ffdcc
witaway <witaway33@gmail.com>
2022-09-24 15:37:25 +0300

    Reorganized project structure
    >>> Divided /src to /src/main and /src/renderer folders. Great for start.
    
    >>> Fixed config files so webpack can build it and vscode highlight successfully.

--- src/renderer/views/HomeView.vue
+++ src/renderer/views/HomeView.vue
@@ -7,12 +7,10 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default defineComponent({
   name: "HomeView",
-  components: {
-    HelloWorld,
-  },
+  components: { HelloWorld },
 });
 </script>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,25 @@
 const { defineConfig } = require("@vue/cli-service");
+const path = require("path");
+
+const vueSrc = "./src/renderer";
+
 module.exports = defineConfig({
   transpileDependencies: true,
+  pages: {
+    index: {
+      entry: "src/renderer/main.ts",
+    },
+  },
+  pluginOptions: {
+    electronBuilder: {
+      mainProcessFile: "src/main/index.ts",
+    },
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        "@": path.join(__dirname, vueSrc),
+      },
+    },
+  },
 });
